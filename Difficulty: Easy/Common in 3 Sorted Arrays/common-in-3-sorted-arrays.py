324948f38d73aadc2dcf75185c4001cfe0e0edd0
palaksingh87 <palak.singh.cs28@iilm.edu>
2026-05-08 22:26:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.py
@@ -0,0 +1,31 @@
+class Solution:
+    def commonElements(self, a, b, c):
+        i = j = k = 0
+        n1, n2, n3 = len(a), len(b), len(c)
+
+        result = []
+
+        while i < n1 and j < n2 and k < n3:
+
+            # If all elements are equal
+            if a[i] == b[j] == c[k]:
+
+                # Avoid duplicates
+                if not result or result[-1] != a[i]:
+                    result.append(a[i])
+
+                i += 1
+                j += 1
+                k += 1
+
+            # Move the pointer with smallest value
+            elif a[i] < b[j]:
+                i += 1
+
+            elif b[j] < c[k]:
+                j += 1
+
+            else:
+                k += 1
+
+        return result
