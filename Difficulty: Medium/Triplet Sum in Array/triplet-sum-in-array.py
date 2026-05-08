788d01a6f7d984a892839f3134f9686b3ed5280f
palaksingh87 <palak.singh.cs28@iilm.edu>
2026-05-08 22:32:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Triplet Sum in Array/triplet-sum-in-array.py
@@ -0,0 +1,23 @@
+class Solution:
+    def hasTripletSum(self, arr, target):
+        n = len(arr)
+
+        # Sort the array
+        arr.sort()
+
+        # Fix one element and use two pointers
+        for i in range(n - 2):
+            left = i + 1
+            right = n - 1
+
+            while left < right:
+                curr_sum = arr[i] + arr[left] + arr[right]
+
+                if curr_sum == target:
+                    return True
+                elif curr_sum < target:
+                    left += 1
+                else:
+                    right -= 1
+
+        return False
